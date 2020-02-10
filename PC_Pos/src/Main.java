faddba96369d37731c7eb54a68c53d2a734d5252
Suebeen <1003jamie@naver.com>
2020-02-10 18:34:07 +0900

    Revert "mune test"
    This reverts commit db8162074d9cb808b44dcc402ad7697a4c259141.

--- PC_Pos/src/Main.java
+++ PC_Pos/src/Main.java
@@ -1,12 +1,11 @@
 import Home.*;
-import Menu.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		//new ShowHome();
-		new ShowMenu();
+		new ShowHome();
+		
 		//이게 된다면 쏴리 질러~~!~!~!~!
 	}
 
--- PC_Pos/src/Menu/ShowMenu.java
+++ PC_Pos/src/Menu/ShowMenu.java
@@ -1,32 +1,9 @@
 package Menu;
 
-import java.awt.*;
-
-import javax.swing.*;
-
-public class ShowMenu extends JFrame{
+public class ShowMenu {
 	//메뉴전체출력(메인,토핑)
 	//선택한 메뉴 list<String>로 저장
 	//결제버튼 누르면 list를 매개변수로 PayMenu
-	int scH,scW;
-	public ShowMenu(){
-		super("메뉴");
-		Toolkit kit = this.getToolkit();
-		Dimension sc = kit.getScreenSize();
-		scH = sc.height;
-		scW = sc.width;
-		this.setLocation(scW/6,scH/6);
-		this.setSize(scW-scW/3,scH-scH/3);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		menuImg();
-		this.setVisible(true);
-	}
-	private void menuImg() {
-		// TODO Auto-generated method stub
-		JPanel food = new JPanel();
-		
-		
-	}
-
+	
 
 }
