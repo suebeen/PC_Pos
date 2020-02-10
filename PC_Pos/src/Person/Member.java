b08778213d82cf5aad8dfac7e57374e7275298b1
Suebeen <1003jamie@naver.com>
2020-02-10 14:21:42 +0900

    2/10 commit1

--- PC_Pos/src/Person/Member.java
+++ PC_Pos/src/Person/Member.java
@@ -1,15 +1,12 @@
 package Person;
 
-public class Member{
+public class Member extends Person{
 	
 	String name;	//이름
-	String ID;	//ID(전화번호)
 	String birth;	//생년월일
 	int totalTime;	//누적시간
-	int restTime;	//잔여시간
 	String grade;	//등급
 	boolean isAdult;	//미성년자확인(01년생부터성인)
-	double saleRatio;	//할인률
 	
 	//constructor
 	public Member() {	//default
---
+++ PC_Pos/src/Person/Person.java
@@ -0,0 +1,8 @@
+package Person;
+
+public class Person {
+
+	String ID;	//ID
+	int restTime;	//ÀÜ¿©½Ã°£
+	double saleRatio;	//ÇÒÀÎ·ü
+}
