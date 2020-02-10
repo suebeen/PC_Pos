db8162074d9cb808b44dcc402ad7697a4c259141
정덕수 <wet5754@naver.com>
2020-02-10 18:18:02 +0900

    mune test

--- PC_Pos/src/Main.java
+++ PC_Pos/src/Main.java
@@ -1,11 +1,12 @@
 import Home.*;
+import Menu.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		new ShowHome();
-		
+		//new ShowHome();
+		new ShowMenu();
 		//이게 된다면 쏴리 질러~~!~!~!~!
 	}
 
--- PC_Pos/src/Menu/ShowMenu.java
+++ PC_Pos/src/Menu/ShowMenu.java
@@ -1,9 +1,32 @@
 package Menu;
 
-public class ShowMenu {
+import java.awt.*;
+
+import javax.swing.*;
+
+public class ShowMenu extends JFrame{
 	//메뉴전체출력(메인,토핑)
 	//선택한 메뉴 list<String>로 저장
 	//결제버튼 누르면 list를 매개변수로 PayMenu
-	
+	int scH,scW;
+	public ShowMenu(){
+		super("메뉴");
+		Toolkit kit = this.getToolkit();
+		Dimension sc = kit.getScreenSize();
+		scH = sc.height;
+		scW = sc.width;
+		this.setLocation(scW/6,scH/6);
+		this.setSize(scW-scW/3,scH-scH/3);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menuImg();
+		this.setVisible(true);
+	}
+	private void menuImg() {
+		// TODO Auto-generated method stub
+		JPanel food = new JPanel();
+		
+		
+	}
+
 
 }
